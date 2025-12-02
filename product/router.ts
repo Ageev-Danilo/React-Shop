4e91dedbafb7ed03aa9a16ad841c210dd400b85a
Ageev-Danilo <danilaageev09@gmail.com>
2025-12-02 20:57:11 +0100

    deleted bad commit

--- product/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { productController } from "./product.controller";
-
-const router = Router();
-
-router.get("/", productController.getAll);
-router.get("/:id", productController.getById);
-
-export default router;
