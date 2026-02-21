3bb306aeb2a33657a55baa059f7e5a98a6193565
Ageev-Danilo <danilaageev09@gmail.com>
2026-02-21 18:11:05 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { productRouter } from "./product/product.router";
 import { categoryRouter } from './category/category.router';
 import { mailRouter } from "./main/mail.router";
 import {userRouter} from "./user/user.router"
+import cors from "cors"
 
 const app: express.Express = express()
 
@@ -11,6 +12,10 @@ app.use(productRouter)
 app.use("/users/profile", categoryRouter)
 app.use("/mail", mailRouter);
 app.use(userRouter)
+
+app.use(cors({
+    origin: ["http://localhost:3000"]
+}))
 
 app.listen(8000, 'localhost', () => {
     console.log('http://localhost:8000')
