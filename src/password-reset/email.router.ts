389de47ce41c66b75cab3aa1f18ae72ef80328e8
Ageev-Danilo <danilaageev09@gmail.com>
2026-03-02 19:11:17 +0100

    fixed email

--- src/password-reset/email.router.ts
+++ src/password-reset/email.router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { emailController } from './email.controller';
 import { validateResetTokenMiddleware } from '../middlewares/validateResetToken.middleware';
 
+
 export const emailRouter = Router();
 
 emailRouter.post('/forgot-password', emailController.sendResetLink);
