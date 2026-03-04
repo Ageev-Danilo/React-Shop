a46c62c51bec9768a9d2e3db568ec209ed4a3c4d
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2026-03-04 17:51:17 +0200

    'update'

--- src/order/order.types.ts
+++ src/order/order.types.ts
@@ -15,16 +15,23 @@ export interface Order {
     [key: string]: any;
 }
 
-export interface OrderServiceContract {
+export interface orderServiceContract {
     addProductToOrder(orderId: number, productData: ProductData): Promise<any>;
     getProductsInOrder(orderId: number): Promise<any>;
     createOrder(userId: number, orderData: OrderData): Promise<Order>;
     getOrderById(orderId: number): Promise<Order>;
 }
 
-export interface OrderControllerContract {
+export interface orderControllerContract {
     addProductToOrder(req: any, res: any): Promise<void>;
     getProductsInOrder(req: any, res: any): Promise<void>;
     createOrder(req: any, res: any): Promise<void>;
     getOrderById(req: any, res: any): Promise<void>;
+}
+
+export interface orderRepositoryContract {
+    addProductToOrder(orderId: number, productData: ProductData): Promise<any>;
+    getProductsInOrder(orderId: number): Promise<any>;
+    createOrder(userId: number, orderData: OrderData): Promise<Order>;
+    getOrderById(orderId: number): Promise<Order>;
 }
