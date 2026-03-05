64f38f90549d8b4ac46f889f4e582ed5ee5ecbba
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2026-03-05 20:18:58 +0200

    update

--- src/scripts/push.js
+++ src/scripts/push.js
@@ -1,11 +1,14 @@
 const { execSync } = require('child_process');
 
-const message = process.argv.slice(2).join(' ') || 'update';
+const args = process.argv.slice(2);
+
+const branch = args.pop() || 'main';
+const message = args.join(' ') || 'update';
 
 try {
     execSync('git add .', { stdio: 'inherit' });
     execSync(`git commit -m "${message}"`, { stdio: 'inherit' });
-    execSync('git push origin main', { stdio: 'inherit' });
+    execSync(`git push origin ${branch}`, { stdio: 'inherit' });
 } catch (err) {
     process.exit(1);
 }
